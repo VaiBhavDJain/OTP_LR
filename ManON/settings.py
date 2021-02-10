38a363a51aedb710d39652ef0d5292502de3f136
VaiBhavDJain <70593112+VaiBhavDJain@users.noreply.github.com>
2021-02-10 16:29:42 +0530

    Update settings.py

--- ManON/settings.py
+++ ManON/settings.py
@@ -144,4 +144,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'vaibhav.cubexo@gmail.com'
-EMAIL_HOST_PASSWORD = "Vaibhav@4997"
+EMAIL_HOST_PASSWORD = "V"
